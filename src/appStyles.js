709dc13d6ec68f79da9cddc12f2c9051a964d1f0
Chris Stoddart <cstoddart@rocketmail.com>
2018-12-21 06:40:36 +0000

    added assets

--- src/appStyles.js
+++ src/appStyles.js
@@ -2,19 +2,10 @@ import styled, { createGlobalStyle } from 'styled-components';
 import Particles from 'react-particles-js';
 
 import { SITE_WIDTH } from './constants';
-import bitter from './assets/fonts/Bitter-Regular.ttf';
 import bitterBold from './assets/fonts/Bitter-Bold.ttf';
-import montserrat from './assets/fonts/Montserrat-Regular.ttf';
 import montserratMedium from './assets/fonts/Montserrat-Medium.ttf';
-import montserratBold from './assets/fonts/Montserrat-Bold.ttf';
 
 export const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: Bitter;
-    font-weight: 400;
-    src: url(${bitter});
-  }
-
   @font-face {
     font-family: Bitter;
     font-weight: 700;
@@ -23,20 +14,8 @@ export const GlobalStyles = createGlobalStyle`
 
   @font-face {
     font-family: Montserrat;
-    font-weight: 400;
-    src: url(${montserrat});
-  }
-
-  @font-face {
-    font-family: Montserrat;
     font-weight: 500;
     src: url(${montserratMedium});
-  }
-
-  @font-face {
-    font-family: Montserrat;
-    font-weight: 700;
-    src: url(${montserratBold});
   }
 
   html, body, #root {
